c86b4243b1e1456fc3383c78037a78c16122d45d
jmurp7385 <jmurp7385@gmail.com>
2017-10-14 01:12:47 -0500

    db stuff-orders per dept

--- api/api.py
+++ api/api.py
@@ -9,6 +9,52 @@ import json
 # engine = sa.create_engine('sqlite:///database.db')
 # Base.metadata.create_all(engine)
 
+dept = {
+  1 : 'Frozen',
+  2 : 'Health',
+  3 : 'Grains',
+  4 : 'Fruits & Veggies',
+  5 : 'Alcohol',
+  6 : 'Foriegn',            #asian and mexican
+  7 : 'Beverages',
+  8 : 'Pet Food',
+  9 : 'Pasta & Rice',       #sauces, pasta, rice
+  10: 'Fruits & Veggies',
+  11: 'Toiletries',
+  12: 'Meats',
+  13: 'Condiments',         #biscuits
+  14: 'Breakfast',          #granola and such
+  15: 'Canned Foods',
+  16: 'Dairy',
+  17: 'Cleaning Supplies',
+  18: 'Baby Food',
+  19: 'Snacks',             #chips, crackers, craisins, etc
+  20: 'Deli',               #deli stuff, salads, meat
+  21: 'Misc'
+}
+
+# dept['1']  = 'Frozen'
+# dept['2']  = 'Health'
+# dept['3']  = 'Grains'
+# dept['4']  = 'Fruits & Veggies'
+# dept['5']  = 'Alcohol'
+# dept['6']  = 'Foriegn' #asian and mexican
+# dept['7']  = 'Beverages'
+# dept['8']  = 'Pet Food'
+# dept['9']  = 'Pasta & Rice' #sauces, pasta, rice
+# dept['10'] = 'Fruits & Veggies'
+# dept['11'] = 'Toiletries'
+# dept['12'] = 'Meats'
+# dept['13'] = 'Condiments' #biscuits
+# dept['14'] = 'Breakfast' #granola and such
+# dept['15'] = 'Canned Foods'
+# dept['16'] = 'Dairy'
+# dept['17'] = 'Cleaning Supplies'
+# dept['18'] = 'Baby Food'
+# dept['19'] = 'Snacks' #chips, crackers, craisins, etc
+# dept['20'] = 'Deli' #deli stuff, salads, meat
+# dept['21'] = 'Misc'
+
 def create_db():
   conn = sqlite3.connect('./db/database.db')
   conn.text_factory = lambda x: unicode(x, "utf-8", "ignore")
@@ -45,19 +91,14 @@ app = Flask(__name__)
 # db = sa(app)
 
 @app.route("/")
-def hello():
+def main():
   print('Python version ' + sys.version)
   conn = sqlite3.connect('./db/database.db')
   c = conn.cursor()
   d = dict()
   for x in range(1,21):
     orders = c.execute("SELECT COUNT(DISTINCT order_number) FROM catalog WHERE department_id = "+str(x))
-    # print(c.fetchall()[0][0])
-    d["dept_"+str(x)]= json.dumps(c.fetchall()[0][0])#orders
-  print(d)
-  # orders = c.execute("SELECT COUNT(DISTINCT order_number) FROM catalog WHERE department_id = 1")
-  # json_string = json.dumps(c.fetchall())
-  # json_string = json.dumps(d)
+    d[dept[x]]= json.dumps(c.fetchall()[0][0])#orders
   return jsonify(d)
 
 @app.route("/city/<string:city>/")
