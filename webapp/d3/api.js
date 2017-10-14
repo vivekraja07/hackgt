e3336e9aa4f01a01d6748d5d43201f6a1e8f62e1
jmurp7385 <jmurp7385@gmail.com>
2017-10-14 10:07:43 -0500

    2 svgs

--- webapp/d3/api.js
+++ webapp/d3/api.js
@@ -1,3 +1,28 @@
+dept = {
+  0 : 'All Orders',
+  1 : 'Frozen',
+  2 : 'Health',
+  3 : 'Grains',
+  4 : 'Fruits & Veggies',
+  5 : 'Alcohol',
+  6 : 'Foreign',
+  7 : 'Beverages',
+  8 : 'Pet Food',
+  9 : 'Pasta & Rice',
+  10: 'Fruits & Veggies',
+  11: 'Toiletries',
+  12: 'Meats',
+  13: 'Condiments',
+  14: 'Breakfast',
+  15: 'Canned Foods',
+  16: 'Dairy',
+  17: 'Cleaning Supplies',
+  18: 'Baby Food',
+  19: 'Snacks',
+  20: 'Deli',
+  21: 'Misc'
+}
+
 function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
@@ -19,7 +44,31 @@ function makeCORSRequest(url) {
     }
     xhr.onload = function() {
         var text = xhr.responseText;
-        alert('Response from CORS request to ' + url + ': ' + text);
+        json = JSON.parse(text);
+        var total = json['All Orders']['Transactions'];
+        var x = 0;
+        var data = [
+            {label: 'Frozen',               value: json['Frozen']['Transactions']/total},
+            {label: 'Health',               value: json['Health']['Transactions']/total},
+            {label: 'Grains',               value: json['Grains']['Transactions']/total},
+            {label: 'Fruits & Veggies',     value: json['Fruits & Veggies']['Transactions']/total},
+            {label: 'Alcohol',              value: json['Alcohol']['Transactions']/total},
+            {label: 'Foreign',              value: json['Foreign']['Transactions']/total},
+            {label: 'Beverages',            value: json['Beverages']['Transactions']/total},
+            {label: 'Pet Food',             value: json['Pet Food']['Transactions']/total},
+            {label: 'Pasta & Rice',         value: json['Pasta & Rice']['Transactions']/total},
+            {label: 'Toiletries',           value: json['Toiletries']['Transactions']/total},
+            {label: 'Meats',                value: json['Meats']['Transactions']/total},
+            {label: 'Condiments',           value: json['Condiments']['Transactions']/total},
+            {label: 'Breakfast',            value: json['Breakfast']['Transactions']/total},
+            {label: 'Canned Foods',         value: json['Canned Foods']['Transactions']/total},
+            {label: 'Dairy',                value: json['Dairy']['Transactions']/total},
+            {label: 'Cleaning Supplies',    value: json['Cleaning Supplies']['Transactions']/total},
+            {label: 'Baby Food',            value: json['Baby Food']['Transactions']/total},
+            {label: 'Snacks',               value: json['Snacks']['Transactions']/total},
+            {label: 'Deli',                 value: json['Deli']['Transactions']/total}
+        ];
+        change(data);
     };
     xhr.onerror = function() {
         alert('Error making the request.');
@@ -28,7 +77,10 @@ function makeCORSRequest(url) {
 }
 
 function getCity(city) {
-    makeCORSRequest("http://localhost:5000/city/" + city)
+    makeCORSRequest("http://localhost:5000/city/" + city);
 }
 
-getCity("NewYork");
+function getTotal(city) {
+    makeCORSRequest("http://localhost:5000/totals");
+}
+
